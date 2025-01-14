ca670ec176c059b6d57c8cb21f792d97e9d646ba
feerfecci <126616983+feerfecci@users.noreply.github.com>
2025-01-14 10:01:57 -0300

    Add files via upload

--- components/home.php
+++ components/home.php
@@ -1,44 +1,49 @@
-<section class="row conteudo aba-home " style="gap: 10px; align-items: center;">
-    <div style="justify-items: center;">
-        <h1>
-            Fernando Amorim Fecci
-        </h1>
-        <div class="subtitle">
-            Desenvolvedor júnior
-        </div class="subtitle">
-        <p> Cientista da computação em formação, buscando ganhar experiência em programação front-end de APPs e
-            sites. Aqui
-            você poderá ver um pouco mais sobre mim e formas de contato. </p>
-        <section class="section-a">
-            <div class="subtitle">Minha redes:</div>
-            <a href="http://github.com/feerfecci">
-                GitHub
-            </a>
-            <a href="https://www.linkedin.com/in/fernando-fecci-71a047193/">
-                Linkedin
-            </a>
-        </section>
-
-    </div>
-    <img class="imagem-perfil" src="./assets/perfil.jpg" alt="">
-</section>
-
-<section class="row row-projeto"  >
-    <div class="projeto">
-        <div class="acessarprojeto">
+<section class="column conteudo  aba-home">
+    <section class="row  " style="gap: 10px; align-items: center;">
+        <div style="justify-items: center;">
+            <h1>
+                Fernando Amorim Fecci
+            </h1>
+            <div class="subtitle">
+                Desenvolvedor júnior
+            </div class="subtitle">
+            <p> Cientista da computação em formação, buscando ganhar experiência em programação front-end de APPs e
+                sites. Aqui
+                você poderá ver um pouco mais sobre mim e formas de contato. </p>
+            <section class="section-a">
+                <div class="subtitle">Minha redes:</div>
+                <a href="http://github.com/feerfecci">
+                    GitHub
+                </a>
+                <a href="https://www.linkedin.com/in/fernando-fecci-71a047193/">
+                    Linkedin
+                </a>
+            </section>
 
         </div>
-    </div>
-    <div class="projeto">
-        <div class="acessarprojeto">
-        <i class="bi bi-plus-circle-dotted" style="font-size: 30px;"></i>
+        <img class="imagem-perfil" src="./assets/perfil.jpg" alt="">
+    </section>
+    <section class="row row-projeto">
+        <div class="projeto">
+            <img class="imagem-perfil" src="./assets/perfil.jpg" alt="" style="width: 100%; height: 100%;">
+
+            <div class="acessarprojeto">++
+            </div>
         </div>
+        <div class="projeto">
+            <img class="imagem-perfil" src="./assets/perfil.jpg" alt="" style="width: 100%; height: 100%;">
 
-    </div>
-    <div class="projeto">
-        <div class="acessarprojeto">
+            <div class="acessarprojeto">
+            </div>
 
         </div>
+        <div class="projeto">
+            <img class="imagem-perfil" src="./assets/perfil.jpg" alt="" style="width: 100%; height: 100%;">
 
-    </div>
+            <div class="acessarprojeto">
+
+            </div>
+
+        </div>
+    </section>
 </section>
